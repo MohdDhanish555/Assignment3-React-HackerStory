347bd9900c858f70bf991d4a98cbedc8e5400e1e
MohdDhanish555 <mahammeddhanish@gmail.com>
2022-05-23 18:52:41 +0530

    optimizations

--- src/pages/RawContent.tsx
+++ src/pages/RawContent.tsx
@@ -1,20 +1,20 @@
-import { Box, Button} from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const RawContent = () => {
   const { state }: any = useLocation();
   const newStory = JSON.stringify(state?.story, null, 4);
-    const navigate = useNavigate();
-    
+  const navigate = useNavigate();
+
   return (
     <>
       <Box display="flex" justifyContent="space-between">
-      <pre>{newStory}</pre>;
+        <pre>{newStory}</pre>
         <Button
           sx={{
             alignSelf: "flex-start",
-            margin : "1rem"
-        }}
+            margin: "1rem",
+          }}
           variant="outlined"
           data-testid="btn-Raw"
           onClick={() => navigate(-1)}
